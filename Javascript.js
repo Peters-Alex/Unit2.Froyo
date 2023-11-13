24ae7d06c52071e306c5a4d42c00c8ee473ece1a
Alex Peters <mr.peters@Alexs-MacBook-Air-2.local>
2023-11-12 21:18:05 -0600

    Step 1 created prompt complete

---
+++ Javascript.js
@@ -0,0 +1,5 @@
+//First thing you need to do is to create a prompt upon opening the website.
+const userInputString = prompt(
+    "Please enter a list of comma-separted froyo flavors.",
+    "vanilla, vanilla, vanilla, strawbery, coffee, coffee"
+  );
